888b67f70b0d62bbf0f8962c186491f45240ea4d
discoduckling <mitalee.n.desai@gmail.com>
2018-02-22 14:46:09 -0700

    Initial commit for client skeleton

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Content goes here
+      </div>
+    );
+  }
+}
+
+export default App;
