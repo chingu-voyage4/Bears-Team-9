ac750e0da537256d63bc5d6b497d25286280173a
discoduckling <mitalee.n.desai@gmail.com>
2018-02-25 10:13:50 -0700

    Start adding Redux wiring

--- client/src/containers/Dashboard.jsx
+++ client/src/containers/Dashboard.jsx
@@ -1,77 +1,10 @@
 import React, { Component } from 'react';
 import Stack from '../components/Stack';
+import { connect } from 'react-redux';
 
 class Dashboard extends Component {
-    state = {
-        stacks: [
-            {
-                category: 'front end',
-                cards: [
-                    {
-                        id: 1,
-                        front: 'What is HTTPS',
-                        back: 'an internet protocol'
-                    },
-                    {
-                        id: 2,
-                        front: 'What is Sass',
-                        back: 'a CSS preprocessor'
-                    },
-                    {
-                        id: 3,
-                        front: 'is react a library or a framework',
-                        back: 'a library'
-                    }
-                ],
-                id: 'stack_1'
-            },
-            {
-                category: 'back end',
-                cards: [
-                    {
-                        id: 1,
-                        front: 'Which side does the back end deal with?',
-                        back: 'the client side'
-                    },
-                    {
-                        id: 2,
-                        front: 'what is state in redux?',
-                        back: 'it stores global variables for a react app'
-                    },
-                    {
-                        id: 3,
-                        front: 'what is the back end language for django apps?',
-                        back: 'python'
-                    }
-                ],
-                id: 'stack_2'
-            },
-            {
-                category: 'history',
-                cards: [
-                    {
-                        id: 1,
-                        front: 'Who was the first American president?',
-                        back: 'George Washington'
-                    },
-                    {
-                        id: 2,
-                        front: "What was Alexander Hamilton's role as a founding father?",
-                        back: 'He fought under Washington and was the first treasury secretary'
-                    },
-                    {
-                        id: 3,
-                        front: 'Which country was the first to send a man into space?',
-                        back: 'Soviet Union'
-                    }
-                ],
-                id: 'stack_3'
-            }
-        ],
-        currentStackId: 'stack_1'
-    }
     render () {
-        const stacks = this.state.stacks.map(stack => {
+        const stacks = this.props.stacks.map(stack => {
             return (
                 <Stack 
                     name={ stack.category }
@@ -86,4 +19,9 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard;
+const mapStateToProps = state => {
+    return {
+        stacks: state.card.stacks
+    }
+}
+export default connect(mapStateToProps)(Dashboard);
---
+++ client/src/store/actions.js
@@ -0,0 +1,3 @@
+export const ADD_CARD = 'ADD_CARD';
+export const DELETE_CARD = 'DELETE_CARD';
+export const CHANGE_STACK = 'CHANGE_STACK';
