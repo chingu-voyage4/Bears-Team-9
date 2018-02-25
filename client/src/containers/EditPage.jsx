300fdac2eb8f733cb5a8b3994c1db358dd740706
discoduckling <mitalee.n.desai@gmail.com>
2018-02-24 19:03:46 -0700

    Add routing for React app

---
+++ client/src/containers/EditPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditPage extends Component {
+    render () {
+        return (
+            <div className="temporary">
+                Edit Page
+            </div>
+        );
+    }
+}
+
+export default EditPage;
