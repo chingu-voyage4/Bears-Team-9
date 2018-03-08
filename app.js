97e34c9703f3e44ea30baee2656de64a671b9369
Bigghead <shavenchew@yahoo.com>
2018-03-07 17:08:31 -0800

    Auth0 boilerplate

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ const logger  = require( 'morgan' );
 const path    = require( 'path' );
 const PORT    = process.env.PORT || 3001;
 const util    = require( './server/helpers/seedDB' )
-const bodyParser = require( 'body-parser' );
-const mongoose   = require( 'mongoose' );
-const serveStatic = require( 'serve-static' );
+const bodyParser     = require( 'body-parser' );
+const mongoose       = require( 'mongoose' );
+const serveStatic    = require( 'serve-static' );
+const passportConfig = require( './server/passport-config' );
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use( passportConfig );
 
 
 // ===== MODELS ===== //
