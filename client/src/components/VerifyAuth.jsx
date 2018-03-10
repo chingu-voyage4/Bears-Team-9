9514179aa745ee8cf42743b2f90d2172303535b3
Bigghead <shavenchew@yahoo.com>
2018-03-09 19:08:39 -0800

    adding in user reducer

---
+++ client/src/components/VerifyAuth.jsx
@@ -0,0 +1,17 @@
+import { React, Component } from 'react';
+
+class VerifyAuth extends Component {
+
+    componentDidMount() {
+        fetch( '/auth/checkAuth', { credentials: 'include' } )
+            .then( res => res.json() )
+            .then( res => console.log( res ) ) 
+            .catch( err => console.log( err ) )
+    }
+
+    render(){
+        return null;
+    }
+}
+
+export default VerifyAuth
