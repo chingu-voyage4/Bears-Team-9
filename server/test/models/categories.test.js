7359f50e9141fcd51dbb43f137c6bbeaf2529003
Bigghead <shavenchew@yahoo.com>
2018-03-20 22:00:40 -0700

    adding basic test assertions

--- server/test/models/categories.test.js
+++ server/test/models/categories.test.js
@@ -5,9 +5,13 @@ const userId     = '101010101010101010101010';
 const Users      = require( '../../models/users' );
 const Categories = require( '../../models/categories' );
 
+
 const mongoose   = require( 'mongoose' );
 mongoose.Promise = global.Promise;
-mongoose.connect( `mongodb://Bears-09:hellohello>@ds031902.mlab.com:31902/bears-09-test` )
+mongoose.connect( `mongodb://Bears-09:hellohello@ds031902.mlab.com:31902/bears-09-test` )
+
+const catController = require( '../../controllers/categories' );
+
 
 
 const dummyCategory = {
@@ -19,6 +23,36 @@ const dummyCategory = {
 }
 
 
-describe( 'getAll' , () => {
+describe( 'Categories model', () => {
+    
+    before( async done => {
 
-})
+        try {
+            await Categories.create( dummyCategory );
+            done();
+        } catch ( err ) {
+            console.log( 'Unable to create category' );
+            process.exit( 1 );
+        }
+    } );
+
+    after( async done => {
+
+        try {
+            await Categories.remove( {} );
+            done();
+        } catch ( err ) {
+            console.log( 'Unable to remove categories' );
+            process.exit( 1 );
+        }
+    } )
+} );
+
+
+
+describe( 'getAll function' , () => {
+
+    it( 'should return a function' , () => {
+        assert.isFunction( catController.getAll );
+    } )
+} )
